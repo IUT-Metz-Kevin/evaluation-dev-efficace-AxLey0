dc93e8e677728ce048237adf83b14ff7dfba7ec2
AxLey <alexglay57@hotmail.com>
2025-06-03 13:55:04 +0200

    Del minesweeper.test.ts

--- minesweeper.ts
+++ minesweeper.ts
@@ -1,3 +1,17 @@
+import { assertEquals } from "jsr:@std/assert";
+
 export function minesweeper(input : string): string{
     return input === "*" ? "*" : "0";
 }
+
+Deno.test("Grille 1x1", () => {
+    assertEquals(minesweeper("."),"0");
+});
+
+Deno.test("Grille 1x1 avec mine", () => {
+    assertEquals(minesweeper("*"),"*");
+});
+
+Deno.test("Ligne simple", () => {
+    assertEquals(minesweeper(".*."),"1*1");
+});
